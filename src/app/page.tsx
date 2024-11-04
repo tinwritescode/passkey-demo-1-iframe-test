93ab62a1170139b49808763470b1a690e953a958
Tin Nguyen <tinnguyen@Tins-MacBook-Pro.local>
2024-11-04 14:22:03 +0700

    add example app

---
+++ src/app/page.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { env } from "../env";
+
+export default function Home() {
+  return (
+    <div className="mx-auto max-w-xl">
+      <div className="text-2xl font-bold">Testing cross domain</div>
+
+      {/* iframe 3000 */}
+      <div className="flex h-screen items-center justify-center">
+        <iframe
+          src={`${env.NEXT_PUBLIC_IFRAME_DOMAIN}`}
+          width="500px"
+          height="500px"
+          className="rounded-md border border-red-400"
+          allow="publickey-credentials-create"
+        />
+      </div>
+    </div>
+  );
+}
